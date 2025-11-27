392830a92c6eab80c53d448dd705d0c14dd4434d
Jakub <jakubgorzny@icloud.com>
2025-11-27 07:35:56 +0100

    feat: channel create/delete logging

---
+++ src/events/channelDelete.ts
@@ -0,0 +1,25 @@
+import { EmbedBuilder, Events, GuildChannel } from 'discord.js';
+
+export default {
+    name: Events.ChannelDelete,
+    once: false,
+    async execute(channel: GuildChannel) {
+        if (!channel.guild) return;
+
+        const logChannel = channel.guild.channels.cache.get(
+            Bun.env.logChannelId,
+        );
+
+        const embedMessage = new EmbedBuilder()
+            .setColor('Greyple')
+            .setAuthor({
+                name: `⏫ Channel created`,
+            })
+            .setDescription(`<#${channel.id}>`)
+            .setTimestamp();
+
+        if (logChannel?.isTextBased()) {
+            logChannel.send({ embeds: [embedMessage] });
+        }
+    },
+};
